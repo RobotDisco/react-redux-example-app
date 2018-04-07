13f468f4fefc67d892e03534c781421aa8fd5cab
Purvi Kanal <purvikanal92@gmail.com>
2018-04-06 22:10:57 -0400

    fetch posts from the API

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import React, { Component } from "react";
 
 import "./App.css";
 import { Posts } from "./components/Posts";
-import rawData from "./data/posts.json";
 
-// get our data into a more friendly format
-const posts = rawData.data.children.map(post => post.data);
+// fetch data from the reddit API
+const getPosts = () =>
+  fetch("https://www.reddit.com/r/asoiaf.json")
+    .then(res => res.json())
+    .then(rawData => rawData.data.children.map(post => post.data));
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+  componentDidMount() {
+    getPosts().then(posts => {
+      this.setState({ posts });
+    });
+  }
   render() {
     return (
       <div>
         <div className="posts-container">
-          <Posts posts={posts} />
+          <Posts posts={this.state.posts} />
         </div>
       </div>
     );
