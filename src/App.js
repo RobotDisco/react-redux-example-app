b88313daeeb91c015abb52346950d87e9ec3774b
Gaelan D'costa <gdcosta@gmail.com>
2018-04-14 22:03:25 -0400

    Added home page (straight out of bridge sample)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import { Subreddit } from "./containers/Subreddit";
+import { Home } from "./containers/Home";
 
 class App extends Component {
   render() {
     return (
       <Switch>
-        <Route path="/r/:name" component={Subreddit}/>
+        <Route exact path="/" component={Home} />
+        <Route path="/r/:name" component={Subreddit} />
       </Switch>
     );
   }
---
+++ src/containers/Home.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subreddit: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ subreddit: event.target.value});
+  }
+
+  goToSubreddit = () => {
+    this.props.history.push(`/r/${this.state.subreddit}`);
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={this.handleChange}/>
+        <button onClick={this.goToSubreddit}>GO</button>
+      </div>
+    );
+  }
+}
