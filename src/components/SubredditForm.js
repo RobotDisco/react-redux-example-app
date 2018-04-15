ec0e20bcc45d66f582f97ef6854779a2855e2da4
Gaelan D'costa <gdcosta@gmail.com>
2018-04-15 00:46:39 -0400

    Moved Home presentation component elsewhere, kept the dodgy name.

---
+++ src/components/SubredditForm.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+/* This is the point where I go completely off the rails and wind up generating
+ * a react? redux? library that consists entirely of "dumb" form components with
+ * an incredibly naive 1:1 mapping to form input widgets.
+ * This is also where I then probably realize that either forms wreak havoc on
+ * Redux state->prop model or that well-designed state structures & mappings
+ * wind up making this really easy as long as you define the appropriate value
+ * changer actions and submit actions.
+ */
+/*
+ * Oh dear shai-hulud how would the submit action know about all the input
+ * values that were sliced up and distributed into different components--oh
+ * wait a minute the submit function would call an action and that action would
+ * have access to the state so as long as there was a section for the form I suppose * this is ok although in a complicated web app this seems a bit dodgy but maybe
+ * ... ok this is above my understanding so I'll just run with it.
+ */
+export const SubredditForm = ({subreddit, changeSubreddit}) => (
+  <div>
+    <input onChange={(event) => changeSubreddit(event.target.value)}/>
+    <button>GO</button>
+  </div>
+);
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,7 @@
-import React from 'react';
-
 import { connect } from "react-redux";
 
 import { changeSubreddit } from "../actions";
-//import { SubredditForm } from "../components/SubredditForm";
+import { SubredditForm } from "../components/SubredditForm";
 
 const mapStateToProps = state => ({
   subreddit: state.subreddit
@@ -12,13 +10,5 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = {
   changeSubreddit
 };
-
-export const SubredditForm = ({subreddit, changeSubreddit}) => (
-  <div>
-    <input onChange={(event) => changeSubreddit(event.target.value)}/>
-    <button>GO</button>
-  </div>
-);
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(SubredditForm);
