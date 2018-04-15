688104d5f4bdff3380849e87448c70e906fff02b
Gaelan D'costa <gdcosta@gmail.com>
2018-04-14 21:17:04 -0400

    Fetch reddit data from reddit itself. Hardcoded subreddit.

--- src/containers/Subreddit.js
+++ src/containers/Subreddit.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import { Posts } from '../components/Posts';
 import './Subreddit.css';
-import seed from '../data/posts.json';
 
 // fetch data from the (fake) Reddit API
-const getPosts = () =>
-      seed.data.children.map(post => post.data);
-
-     /* fetch('../data/posts.json')
+const getPosts = name =>
+     fetch(`https://www.reddit.com/r/${name}.json`)
       .then(res => res.json())
-      .then(rawData => rawData.data.children.map(post => post.data));*/
+      .then(rawData => rawData.data.children.map(post => post.data));
 
 export class Subreddit extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       posts: []
     };
   }
 
   componentDidMount() {
-    //getPosts().then(posts => {
-    this.setState({ posts: getPosts() });
-    //});
+    // const { match } = this.props;
+    getPosts(/* match.params.name*/ 'toronto').then(posts => {
+      this.setState({ posts });
+    });
   };
 
   render() {
